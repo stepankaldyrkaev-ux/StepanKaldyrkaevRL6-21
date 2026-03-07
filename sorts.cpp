a7e7b9078b858fd87588fe626f59e5531139951f
Stepan <kaldyrkaev.stepan@gmail.com>
2026-03-07 14:09:21 +0300

    реализация SelectionSort

--- sorts.cpp
+++ sorts.cpp
@@ -8,4 +8,10 @@ int getMin(int*ar, int size){
 	}
 	return ind_min;
 }
+void selectionSort(int* ar, unsigned int size, bool asc){
+	for(int i=0;i<size;i++){
+		int ind_Min = getMin(&ar[asc==true? i : 0],size - i);
+		std::swap(ar[asc==true? i : size - i - 1], ar[asc==true? ind_Min + i : ind_Min]);
+	}
+}	
 	
