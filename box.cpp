6cef4825bdeedd04b93500a0031eb54cf39ac2bb
Kaldyrkaev_Stepan <kaldyrkaev.stepan@gmail.com>
2026-04-09 07:54:59 +0300

    последняя версия SW4/RK1

--- box.cpp
+++ box.cpp
@@ -9,14 +9,11 @@ void writeToFile(const char* fileName, const struct Box& box){
 		fclose(file);
 	}
 }
-void readFromFile(const char* fileName, struct Box* box){
-	if(box == NULL){
-		return;
-	}
+void readFromFile(const char* fileName, struct Box& box){
 	FILE *file;
     file = fopen(fileName,"r");
 	if(file){
-		fscanf(file,"%i %i %i %i", &box->_len, &box->_width, &box->_height, &box->_color);
+		fscanf(file,"%i %i %i %i", &box._len, &box._width, &box._height, &box._color);
         fclose(file);
 	}
 }
