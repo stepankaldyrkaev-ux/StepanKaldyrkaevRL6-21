86a469e92a8310ffbaf5232c8e2d4e6ede2810ae
Kaldyrkaev_Stepan <kaldyrkaev.stepan@gmail.com>
2026-04-14 19:26:41 +0300

    ну точно последняя версия SW4/RK1

--- box.h
+++ box.h
@@ -7,27 +7,27 @@ struct Box {
 	int _width;
 	int _height;
 	int _color;
-	int _volume;
+	int volume;
 	Box(){
 		_len = 10;
 		_width = 10;
 		_height = 10;
-		_volume = _len*_width*_height;
+		volume = _len*_width*_height;
 	}
 	Box(int a){
 		_len = a;
 		_width = a;
 		_height = a;
-		_volume = _len*_width*_height;
+		volume = _len*_width*_height;
 	}
 	Box(int a, int b, int c){
 		_len = a;
 		_width = b;
 		_height = c;
-		_volume = _len*_width*_height;
+		volume = _len*_width*_height;
 	}
 	friend ostream& operator<<(std::ostream& out, const Box& box) {
-	out << " len = " << box._len << ", width = " << box._width << ", height = " << box._height << ", volume = "  << box._volume << "\n" << std::endl; 
+	out << " len = " << box._len << ", width = " << box._width << ", height = " << box._height << ", volume = "  << box.volume << "\n" << std::endl; 
 	return out;
 	}
 };
